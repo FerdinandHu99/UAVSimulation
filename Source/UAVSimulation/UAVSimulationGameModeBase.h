8f00d5308666834f3c3898cda58a5982407cbd63
Ferdinand <853712064@qq.com>
2023-06-26 22:26:50 +0800

    feat: 初始化项目，加入AirSim插件

---
+++ Source/UAVSimulation/UAVSimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UAVSimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAVSIMULATION_API AUAVSimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
